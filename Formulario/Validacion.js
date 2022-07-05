c651bd6576b80562728516f7cdf3ebd3f32053b8
unknown <rombri32@gmail.com>
2022-07-05 19:26:12 -0400

    prueba de subida

---
+++ Formulario/Validacion.js
@@ -0,0 +1,62 @@
+function validación(){
+
+    let textoMinimo=document.getElementById("txt_minData")
+    if (textoMinimo.length<3){
+    
+        alert("Debe ingresar un texto mayor a 3 lineas");
+        textoMinimo.focus();
+        return false;
+    }
+
+
+    let nombre = document.getElementById("txt_nombreApellido");
+    let expNombre = /^[A-Za-zÑñÁÉÍÓÚáéíóú]+(\s[A-Za-zÑñÁÉÍÓÚáéíóú]+)*$/;
+    if(!expNombre.test(nombre.value)){
+    
+
+        alert("Verifique su nombre porfavor");
+        nombre.focus();
+        return false;
+    }
+
+
+   
+
+    let rut=document.getElementById("txt_rut")
+    let rutformat = /^\d{8}-\d{1}$/;
+    if(!rut.value.match(rutformat)){
+        alert("Revise su rut");
+        rut.focus();
+        return false;
+    }
+
+
+
+
+
+    let telefono=document.getElementById("txt_numero")
+    let telefonoFormat= /^\d{9}$/;
+    if (!telefono.value.match(telefonoFormat)){
+    
+        alert("Movil no valido")
+        telefono.focus();
+        return false;
+    }
+
+
+
+    let compañiacel=document.getElementById("txt_compañia")
+    if (compañiacel.length==0){
+    
+        alert("Debe seleccionar una opción de compañia de cel")
+        compañiacel.focus();
+        return false;
+    }
+    
+   
+    alert("Enviado!")
+
+
+}
+
+
